d0aa646103ceac404137de770ea6099073ac426a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-11 11:42:40 +0000

    Deploy fusebox13/window to github.com/fusebox13/window.git:gh-pages

---
+++ precache-manifest.c936cac5a12d5f4912e7a7cca26973fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6329e0c4fbb9909e202",
+    "url": "/window/js/about.306624fb.js"
+  },
+  {
+    "revision": "3801775b4ee3a6d17834",
+    "url": "/window/css/app.2d2d1643.css"
+  },
+  {
+    "revision": "3801775b4ee3a6d17834",
+    "url": "/window/js/app.9f94df86.js"
+  },
+  {
+    "revision": "d83d49081303d7274083",
+    "url": "/window/js/chunk-vendors.0284c43b.js"
+  },
+  {
+    "revision": "b2eb32cd19bcdc6c853c715e31188716",
+    "url": "/window/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/window/robots.txt"
+  }
+];
